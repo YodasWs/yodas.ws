aa4edc32b08511f51cda9f9b5aa5f1b700df6945
Sam Grundman <samuel@yodas.ws>
2016-01-20 17:47:07 -0500

    Remove bad line

--- index.php
+++ index.php
@@ -40,7 +40,6 @@ default:
 	$page = trim($_SERVER['REQUEST_URI'], '/');
 	if (strstr($page, '/') === false) {
 		$xml = $blog->loc($page);
-			print "<h1>" . urldecode($page) . "</h1>";
 		if (!empty($xml)) {
 			print "<h1>" . urldecode($page) . "</h1>";
 			print '<pre>' . print_r($xml, true) . '</pre>';
