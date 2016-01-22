2e6550e3447eeb06c8df24a217bf4406a531dd9f
Sam Grundman <samuel@yodas.ws>
2016-01-22 17:01:40 -0500

    Initialize List of Top Places only when requested

--- components/world_map/world_map.php
+++ components/world_map/world_map.php
@@ -4,7 +4,7 @@ require_once("components/component.php");
 class WorldMap implements Component {
 
 	private static $self;
-	private $top_places;
+	private $top_places = array();
 	private $xml;
 
 	public static function html() {
@@ -34,25 +34,29 @@ class WorldMap implements Component {
 
 	public function __construct() {
 		$this->xml = simplexml_load_file('world.xml');
-		$this->top_places = array();
-		foreach ($this->xml->locale as $l) {
-			$this->top_places[] = $l;
-		}
-		uasort($this->top_places, function($a, $b) {
-			if (empty($a->img) and !empty($b->img)) return 1;
-			if (!empty($a->img) and empty($b->img)) return -1;
-			if (!empty($a->img) and !empty($b->img)) {
-				if (count($a->img) == count($b->img)) return 0;
-				return (count($a->img) < count($b->img)) ? 1 : -1;
-			}
-			return 0;
-		});
 	}
 
 	public function __get($var) {
 		if (in_array($var, array(
-			'top_places',
 		))) return $this->$var;
+		switch ($var) {
+		case 'top_places':
+			if (!empty($this->top_places)) return $this->top_places;
+			$this->top_places = array();
+			foreach ($this->xml->locale as $l) {
+				$this->top_places[] = $l;
+			}
+			uasort($this->top_places, function($a, $b) {
+				if (empty($a->img) and !empty($b->img)) return 1;
+				if (!empty($a->img) and empty($b->img)) return -1;
+				if (!empty($a->img) and !empty($b->img)) {
+					if (count($a->img) == count($b->img)) return 0;
+					return (count($a->img) < count($b->img)) ? 1 : -1;
+				}
+				return 0;
+			});
+			return $this->top_places;
+		}
 	}
 
 }
