3fd16e83916ea22e6166e4069a6718e2a2c3d4a7
Sam Grundman <samuel@yodas.ws>
2016-01-24 12:45:37 -0500

    Don't expand all HTMLElement, only the Tiles

--- components/tile/tile.js
+++ components/tile/tile.js
@@ -1,26 +1,27 @@
 Math.randInt = function(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min
 }
-HTMLElement.prototype.changeImage = function() {
+window.Tile = function(){}
+Tile.prototype.changeImage = function() {
 	var $a = $(this),
 		$i = $a.children('img'),
 		i = Number.parseInt($a.data('i'), 10) + 1 || 0,
-		alt = $('<div class="alt">'),
 		img
 	$a.find('.alt').remove()
 	if (i == $i.length) i = 0
 	img = $($i[i])
 	if (img.attr('alt')) {
-		alt.text(img.attr('alt')).appendTo($a)
+		$('<div class="alt">').text(img.attr('alt')).appendTo($a)
 	}
 	$a.css({
 		background: 'url(' + img.attr('src') + ') center center no-repeat',
 		backgroundSize: 'cover'
 	}).data('i', i)
-	setTimeout(this.changeImage.bind(this), Math.randInt(5, 20) * 1000)
+	setTimeout(this.tile.changeImage.bind(this), Math.randInt(5, 20) * 1000)
 }
 $(document).ready(function(){
 	$('article.tile').each(function(){
-		this.changeImage()
+		this.tile = new Tile()
+		this.tile.changeImage.call(this)
 	})
 })
