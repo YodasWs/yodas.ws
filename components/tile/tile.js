bdd38d050545a084c26e65b04c4d9f9fba6a3ef0
Sam Grundman <samuel@yodas.ws>
2016-01-28 07:39:54 -0500

    Expand homepage Tiles on click

--- components/tile/tile.js
+++ components/tile/tile.js
@@ -5,23 +5,34 @@ window.Tile = function(){}
 Tile.prototype.changeImage = function() {
 	var $a = $(this),
 		$i = $a.children('img'),
-		i = Number.parseInt($a.data('i'), 10) + 1 || 0,
-		img
+		n = $a.is('.expanded') ? 2 : 1,
+		i = Number.parseInt(this.bg_i, 10) - n + 2 || 0,
+		bg = [],
+		img,k
+	clearTimeout(this.bgto)
 	$a.find('.alt').remove()
-	if (i == $i.length) i = 0
-	img = $($i[i])
-	if (img.attr('alt')) {
-		$('<div class="alt">').text(img.attr('alt')).appendTo($a)
+	for (var j=0; j<n; j++) {
+		if (i == $i.length) i = 0
+		k = i + j
+		while (k >= $i.length) k -= $i.length
+		img = $($i[k])
+		if (img.attr('alt')) {
+			$('<div class="alt">').text(img.attr('alt')).appendTo($a)
+		}
+		bg.push('url(' + img.attr('src') + ')')
 	}
 	$a.css({
-		background: 'url(' + img.attr('src') + ') center center no-repeat',
-		backgroundSize: 'cover'
-	}).data('i', i)
-	setTimeout(this.tile.changeImage.bind(this), Math.randInt(5, 20) * 1000)
+		backgroundImage: bg.join(',')
+	})
+	this.bg_i = i
+	this.bgto = setTimeout(this.tile.changeImage.bind(this), Math.randInt(5, 20) * 1000)
 }
 $(document).ready(function(){
 	$('article.tile').each(function(){
 		this.tile = new Tile()
+		this.tile.changeImage.call(this)
+	}).on('click', function(){
+		$(this).toggleClass('expanded').siblings('.tile.expanded').removeClass('expanded')
 		this.tile.changeImage.call(this)
 	})
 })
