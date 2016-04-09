3a5666f30905ed69ba46add047b11967b0357cff
Sam Grundman <samuel@yodas.ws>
2016-04-09 16:58:50 +0800

    Load Tile images before setting them as Background
    So the user won't see them loading after they've been set as the
    background

--- components/tile/tile.js
+++ components/tile/tile.js
@@ -2,9 +2,22 @@ Math.randInt = function(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min
 }
 window.Tile = function(){}
+Tile.prototype.loadImage = function() {
+	var self = this,
+		$a = $(this),
+		$i = $($a.children('load-img')[0]),
+		img = $i.clone().wrap('<div>').parent().html()
+	if (!img) return
+	img = $(img.replace('load-img', 'img'))
+	img.appendTo($a)
+	img.on('load', function() {
+		$i.remove()
+		self.tile.loadImage()
+	})
+}
 Tile.prototype.changeImage = function() {
 	var $a = $(this),
-		$i = $a.children('img,load-img'),
+		$i = $a.children('img'),
 		n = $a.is('.expanded') ? 2 : 1,
 		i = Number.parseInt(this.bg_i, 10) + 1 || 0,
 		bg = [],
@@ -26,20 +39,23 @@ Tile.prototype.changeImage = function() {
 		backgroundImage: bg.join(',')
 	})
 	this.bg_i = i
-	this.bgto = setTimeout(this.tile.changeImage.bind(this), Math.randInt(5, 20) * 1000)
+	this.bgto = setTimeout(this.tile.changeImage, Math.randInt(5, 20) * 1000)
 }
 $(document).ready(function(){
 	$('article.tile').each(function(){
 		// Instantiate Tile Objects
 		this.tile = new Tile()
-		this.tile.changeImage.call(this)
+		this.tile.loadImage = this.tile.loadImage.bind(this)
+		this.tile.loadImage()
+		this.tile.changeImage = this.tile.changeImage.bind(this)
+		this.tile.changeImage()
 	}).on('click', function(e){
 		var $t = $(this)
 		// Expand/Collapse Tiles
 		if (!$t.is('.expanded')) {
 			// Adjust Background Image
 			this.bg_i = this.bg_i - 1
-			if (this.bg_i < 0) this.bg_i = $t.children('img,load-img').length - 1
+			if (this.bg_i < 0) this.bg_i = $t.children('img').length - 1
 		}
 		// Scroll
 		setTimeout(function() {
@@ -48,7 +64,7 @@ $(document).ready(function(){
 			}, 500, 'swing')
 		}, 500)
 		$t.toggleClass('expanded').siblings('.tile.expanded').removeClass('expanded')
-		this.tile.changeImage.call(this)
+		this.tile.changeImage()
 		$(window).trigger('resize')
 	})
 	// Close Tiles on Click
