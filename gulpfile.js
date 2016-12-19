ce2c6185530d212e2919cc6a1ff95a9428b8435e
Sam Grundman <samuel@yodas.ws>
2016-12-18 19:49:10 -0500

    Let me easily extend gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,44 @@
-"use strict";
+'use strict';
 
 const gulp = require('gulp')
-const sass = require('gulp-sass')
-const concat = require('gulp-concat')
+const plugins = {
+	prefixSass: require('gulp-autoprefixer'),
+	rmLines: require('gulp-delete-lines'),
+	compileSass: require('gulp-sass'),
+	addHeader: require('gulp-header'),
+	concat: require('gulp-concat'),
+}
 
 const options = {
-	sass:{
+	compileSass:{
 		outputStyle:'compressed'
-	}
+	},
+	prefixSass:{
+	},
+	addHeader:(()=> {
+		return "/* <%= file.basename %> */\n"
+	})(),
+	rmLines:{'filters':[
+		'^\s*$',
+	]}
 }
 
 gulp.task('sass', () => {
-	return gulp.src([
+	const tasks = [
+		'compileSass',
+		'prefixSass',
+		'addHeader',
+		'rmLines',
+	]
+	let stream = gulp.src([
 		'layouts/main.css',
 		'{layouts,gtfs,components/*}/*.css',
 		'{layouts,gtfs,components/*}/*.scss'
 	])
-		.pipe(sass(options.sass))
-		.pipe(concat('min.css'))
+	for (let i=0, k=tasks.length; i<k; i++) {
+		stream = stream.pipe(plugins[tasks[i]](options[tasks[i]]))
+	}
+	return stream.pipe(plugins.concat('min.css'))
 		.pipe(gulp.dest('./dist/'))
 })
 
