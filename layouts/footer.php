bf5e0488c43fe2d8d1eea0820d669c2129de40fd
Sam Grundman <samuel@yodas.ws>
2017-01-07 18:54:16 -0500

    Add schema.org markup on copyright year

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer itemscope itemtype="http://schema.org/WPFooter">
-	<small>&copy;2016 Samuel B Grundman</small>
+	<small>&copy;<span itemprop="copyrightYear">2016</span> Samuel B Grundman</small>
 </footer>
 <?php if (file_exists("google_analytics.php")) {
 	include_once("google_analytics.php");
