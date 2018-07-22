4685ecaf4827ca02f57224049ccc59a2f0bca776
Sam Grundman <samuel@yodas.ws>
2018-07-22 14:12:01 -0400

    Init node.js site

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/* app.json */
+angular.module('yodas.ws', modules)
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(false)
+	$routeProvider.when('/', {
+		templateUrl: 'pages/home.html',
+		controllerAs: '$ctrl',
+		controller() {
+			angular.element('[ng-view]').attr('ng-view', 'pageHome')
+		},
+	})
+	.otherwise({redirectTo: '/'})
+}])
