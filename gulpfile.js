4639e2f4882967f2e6cb20b415aa7d9ee9bbaa66
Sam Grundman <samuel@yodas.ws>
2018-07-22 12:04:57 -0400

    Get gulp running to compile and combine Sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,56 @@
 'use strict';
 
-const gulp = require('gulp')
-const plugins = {
-	prefixSass: require('gulp-autoprefixer'),
-	rmLines: require('gulp-delete-lines'),
-	compileSass: require('gulp-sass'),
-	addHeader: require('gulp-header'),
-	concat: require('gulp-concat'),
-}
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')({
+	rename:{
+		'gulp-autoprefixer': 'prefixCSS',
+		'gulp-htmlmin': 'compileHTML',
+		'gulp-babel': 'compileJS',
+		'gulp-sass': 'compileSass',
+	},
+});
 
 const options = {
-	compileSass:{
+	compileSass: {
 		outputStyle:'compressed'
 	},
-	prefixSass:{
+	prefixCSS: {
+		browsers: [
+			'last 2 versions',
+			'Safari 10',
+			'IE 11',
+		],
 	},
-	addHeader:(()=> {
-		return "/* <%= file.basename %> */\n"
-	})(),
 	rmLines:{'filters':[
 		'^\s*$',
-	]}
-}
+	]},
+};
 
 gulp.task('sass', () => {
 	const tasks = [
 		'compileSass',
-		'prefixSass',
-		'addHeader',
+		'prefixCSS',
 		'rmLines',
-	]
+	];
 	let stream = gulp.src([
 		'layouts/main.css',
 		'{layouts,gtfs,components/*}/*.css',
-		'{layouts,gtfs,components/*}/*.scss'
-	])
+		'{layouts,gtfs,components/*}/*.scss',
+	]);
 	for (let i=0, k=tasks.length; i<k; i++) {
-		stream = stream.pipe(plugins[tasks[i]](options[tasks[i]]))
+		stream = stream.pipe(plugins[tasks[i]](options[tasks[i]]));
 	}
 	return stream.pipe(plugins.concat('min.css'))
-		.pipe(gulp.dest('./dist/'))
-})
+		.pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('watch', () => {
-	gulp.watch('./**/*.scss', ['sass'])
-})
+	gulp.watch('./**/*.scss', ['sass']);
+});
 
-gulp.task('default', () => {
-	return gulp.src('./**/*.js')
-})
+gulp.task('default', gulp.parallel(
+	'sass',
+	() => {
+		return gulp.src('./**/*.js');
+	}
+));
